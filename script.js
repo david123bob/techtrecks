6132d6054ae511a1478a3f90f7999d3410e3d79a
David Hao <dhh5827@nyu.edu>
2024-11-11 17:06:43 -0500

    part1

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 //Mabye we want to use API that fetchs amazon information on current screen. Then using that info we write some code that process the info
 //With the info it might use another API to get information about the product like ingeridents etc.
 //Taking the ingredients list then it might use finally another API to get info on these ingredeints and if they are hamrful it will let user know
@@ -21,12 +24,19 @@ const newAmazon=()=>{
     //newDiv=newDiv.querySelector("#detailBulletsWrapper_feature_div")
     //newDiv=newDiv.querySelector("#detailBullets_feature_div")
     bulletList = document.querySelector(".a-unordered-list.a-nostyle.a-vertical.a-spacing-none.detail-bullet-list");
-    upcNumber=bulletList.children[1].textContent.trim()
-    console.log(bulletList)
-    console.log(upcNumber)
-
-
-    //gets the name of product
+    listItems = bulletList.querySelectorAll("li");
+    let upcNumber = null; // Initialize UPC variable
+    listItems.forEach(item => {
+        boldSpan = item.querySelector("span.a-text-bold");
+        // Check if the bold span contains "UPC" and get the associated number
+        if (boldSpan && boldSpan.textContent.includes("UPC")) {
+            const upcNumberElement = boldSpan.nextElementSibling;
+            if (upcNumberElement) {
+                upcNumber = upcNumberElement.textContent.trim();
+            }
+        }
+    });
+    console.log("UPC Number:", upcNumber);
 
     //Sends the information to background.js
     if(upcNumber){
